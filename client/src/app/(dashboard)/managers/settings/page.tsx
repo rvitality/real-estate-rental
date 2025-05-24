78607630d80440cf65ad8d752b0e9eda23ad640f
rvitality <vital.renzjohn@gmail.com>
2025-05-24 19:28:37 +0800

    Add manager settings page and allow user to update their basic details

--- client/src/app/(dashboard)/managers/settings/page.tsx
+++ client/src/app/(dashboard)/managers/settings/page.tsx
@@ -1,7 +1,29 @@
+"use client";
+
 import React from "react";
+import { useGetAuthUserQuery, useUpdateManagerSettingsMutation } from "@/state/api";
+
+import SettingsForm from "@/components/SettingsForm";
 
 const ManagerSettings = () => {
-    return <div>ManagerSettings</div>;
+    const { data: authUser, isLoading } = useGetAuthUserQuery();
+    const [updateManager] = useUpdateManagerSettingsMutation();
+
+    const initialData = {
+        name: authUser?.userInfo.name,
+        email: authUser?.userInfo.email,
+        phoneNumber: authUser?.userInfo.phoneNumber,
+    };
+    const handleSubmit = async (data: typeof initialData) => {
+        await updateManager({
+            cognitoId: authUser?.cognitoInfo?.userId,
+            ...data,
+        });
+    };
+
+    if (isLoading) return <>Loading...</>;
+
+    return <SettingsForm initialData={initialData} onSubmit={handleSubmit} userType='manager' />;
 };
 
 export default ManagerSettings;
