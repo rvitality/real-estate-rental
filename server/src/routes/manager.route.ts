52ef389da1974648a8034668bc8bf72cc57523ea
rvitality <vital.renzjohn@gmail.com>
2025-06-06 13:13:25 +0800

    Add new endpoints in their routes

--- server/src/routes/manager.route.ts
+++ server/src/routes/manager.route.ts
@@ -1,12 +1,13 @@
 import express from "express";
 
 // Controllers
-import { getManager, createManager, updateManager } from "../controllers/manager.controller";
+import { getManager, createManager, updateManager, getManagerProperties } from "../controllers/manager.controller";
 
 const router = express.Router();
 
 router.get("/:cognitoId", getManager);
 router.put("/:cognitoId", updateManager);
+router.put("/:cognitoId/properties", getManagerProperties);
 router.post("/", createManager);
 
 export default router;
--- server/src/routes/tenant.route.ts
+++ server/src/routes/tenant.route.ts
@@ -1,12 +1,13 @@
 import express from "express";
 
 // Controllers
-import { getTenant, createTenant, updateTenant } from "../controllers/tenant.controller";
+import { getTenant, createTenant, updateTenant, getCurrentResidences } from "../controllers/tenant.controller";
 
 const router = express.Router();
 
 router.get("/:cognitoId", getTenant);
 router.put("/:cognitoId", updateTenant);
+router.put("/:cognitoId/current-residences", getCurrentResidences);
 router.post("/", createTenant);
 
 export default router;
